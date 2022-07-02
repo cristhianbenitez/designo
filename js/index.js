9266cb65f34a16434e23cb7544b38994c65c4eca
Cristhian <cristhiantilleria@gmail.com>
2022-07-02 19:48:51 -0400

    a lot of responsive updates

--- js/index.js
+++ js/index.js
@@ -1,15 +1,38 @@
+// Navigation Burger
+const menuBtn = $('.header__navigation-burger');
+const menuList = $('.header__navigation__list');
+$(menuBtn).on('click', () => {
+  menuBtn.toggleClass('open');
+  menuList.toggleClass('visible');
+
+  if ($('body').css('overflow') === 'hidden') {
+    $('html, body').css({
+      overflow: 'visible',
+      height: 'auto'
+    });
+  } else {
+    $('html, body').css({
+      overflow: 'hidden',
+      height: '100%'
+    });
+  }
+});
+
+// Barba Transition
 barba.init({
-  transitions: [{
-    name: 'opacity-transition',
-    leave(data) {
-      return gsap.to(data.current.container, {
-        opacity: 0
-      });
-    },
-    enter(data) {
-      return gsap.from(data.next.container, {
-        opacity: 0
-      });
+  transitions: [
+    {
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
     }
-  }]
+  ]
 });
