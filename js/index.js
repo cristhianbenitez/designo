464674ca8f77bf6d0402dc160e8b827eb04b7195
Cristhian <cristhiantilleria@gmail.com>
2022-07-16 18:02:14 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ $(menuBtn).on('click', () => {
 barba.init({
   transitions: [
     {
-      name: 'default-transition',
+      name: 'default',
       beforeEnter({ current, next, trigger }) {
         window.scrollTo({ top: 0, behavior: 'smooth' });
 
@@ -44,11 +44,90 @@ barba.init({
       },
       enter(data) {
         return gsap.from(data.next.container, {
-          opacity: 0
+          opacity: 1
         });
       }
     }
   ]
 });
 
-// TODO - make that it scroll to top on pages transition
+gsap.registerPlugin(ScrollTrigger);
+// LOCOMOTIVE SCROLL
+const locoScroller = '[data-scroll-container]';
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(locoScroller),
+  smooth: true
+});
+
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on('scroll', ScrollTrigger.update);
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(locoScroller, {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(locoScroller).style.transform
+    ? 'transform'
+    : 'fixed'
+});
+
+let tl = gsap.timeline({});
+
+tl.from('.hero__headline, .hero__copy, .hero__button', {
+  duration: 1,
+  y: 9999,
+  stagger: 0.5
+});
+
+tl.from(
+  '.hero__image',
+  {
+    duration: 2,
+    scale: 0.5,
+    ease: 'power1.out',
+    opacity: 0
+  },
+  '-=1.5'
+);
+
+gsap.from('.categories', {
+  scrollTrigger: {
+    trigger: '.categories',
+    end: 'center center',
+    scroller: locoScroller,
+    scrub: true
+  },
+  opacity: 0,
+  duration: 1,
+  y: 300
+});
+
+gsap.from('.our-process__item', {
+  scrollTrigger: {
+    trigger: '.our-process',
+    scroller: locoScroller,
+    end: 'center center',
+    scrub: true
+  },
+  opacity: 0,
+  duration: 1,
+  x: 500,
+  stagger: 0.5
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ScrollTrigger.addEventListener('refresh', () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
